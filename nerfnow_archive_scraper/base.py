8368f3bb61324de0c362fee440c63a11d1d9d9f6
Michal <michal.rosicki@gmail.com>
2019-07-26 13:45:55 +0200

    nerfnow cleaning

--- nerfnow_archive_scraper/base.py
+++ nerfnow_archive_scraper/base.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
-request = requests.get('https://www.nerfnow.com/archives')
+URL = 'https://www.nerfnow.com/archives'
+request = requests.get(URL)
 soup = BeautifulSoup(request.text, 'html.parser')
 counter = 0
 
@@ -8,25 +9,20 @@ for link in soup.find_all('a'):
     if 'nerfnow.com/comic' in link.get('href'):
         request = requests.get(link.get('href'))
         sub_soup = BeautifulSoup(request.text, 'html.parser')
-        img_tag = sub_soup.find('div', id = 'comic').find('img')
+        img_tag = sub_soup.find('div', id='comic').find('img')
         try:
             img_title = img_tag.attrs['title']
             img_link = img_tag.attrs['src']
-        except:
+        except KeyError:
             img_title = img_tag.contents[0].attrs['title']
             img_link = img_tag.contents[0].attrs['src']
-        
 
-        request = requests.get(img_link, stream = True)
+        request = requests.get(img_link, stream=True)
 
-        with open(str(img_title) + '.jpg','wb') as file:
+        with open(str(img_title) + '.jpg', 'wb') as file:
             for chunk in request:
                 file.write(chunk)
-        counter +=1
+        counter += 1
 
         if counter == 20:
             break
-
-
-
-
